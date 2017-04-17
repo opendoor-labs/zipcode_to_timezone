ff2c236fbbf66d803de8b97ed7e6b95f3117d74a
Daniel Neighman <has.sox@gmail.com>
2017-04-17 13:48:54 -0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule ZipcodeToTimezone.Mixfile do
 
   def project do
     [app: :zipcode_to_timezone,
-     version: "0.1.0",
+     version: @version,
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
